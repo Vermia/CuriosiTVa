c12f45df1f5d0708b4676332408b4add54562e0d
Vermia <33635769+Vermia@users.noreply.github.com>
2017-11-27 20:59:10 +0100

    About con enlace

--- About.java
+++ About.java
@@ -2,6 +2,8 @@ package com.example.vermia.cosas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class About extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class About extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        TextView t2=(TextView) findViewById(R.id.text2);
+        t2.setMovementMethod(LinkMovementMethod.getInstance());
+
     }
 }
