3977d38c4bac46329b9c7324197a35442adc66c5
Vermia <33635769+Vermia@users.noreply.github.com>
2017-12-04 20:24:53 +0100

    series y peliculas
    Desplegable

--- java/MainActivity.java
+++ java/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.pacop.myapplication;
+package com.example.vermia.cosas;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,9 +13,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+
+import static android.view.View.GONE;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,10 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -82,21 +93,39 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_filtros) {
-            // Handle the camera action
-            Intent filtros= new Intent(getApplicationContext(),filtros.class);
+            Intent filtros=new Intent(getApplicationContext(),Filtros.class);
             startActivity(filtros);
         } else if (id == R.id.nav_idiomas) {
             Intent idiomas= new Intent(getApplicationContext(),Idiomas.class);
             startActivity(idiomas);
-        } else if (id == R.id.nav_notif) {
-
         } else if (id == R.id.nav_temas) {
-            Intent temas= new Intent(getApplicationContext(),Temas.class);
+            Intent temas = new Intent(getApplicationContext(),Temas.class);
             startActivity(temas);
+        } else if (id == R.id.nav_about) {
+            Intent about= new Intent(getApplicationContext(),About.class);
+            startActivity(about);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+
+
+    public void despliegaSeries(View v){
+        LinearLayout listaSeries=(LinearLayout)findViewById(R.id.listaSeries);
+
+        if(listaSeries.getVisibility()==GONE)
+            listaSeries.setVisibility(View.VISIBLE);
+        else listaSeries.setVisibility(GONE);
+    }
+
+    public void despliegaPeliculas(View v){
+        LinearLayout listaPeliculas=(LinearLayout)findViewById(R.id.listaPeliculas);
+
+        if(listaPeliculas.getVisibility()==GONE)
+            listaPeliculas.setVisibility(View.VISIBLE);
+        else listaPeliculas.setVisibility(GONE);
     }
 }
