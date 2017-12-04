2eefea47b3e76648c17f757e300eb0fddfbaa4c1
Vermia <33635769+Vermia@users.noreply.github.com>
2017-12-04 21:07:59 +0100

    deberia funcionar

---
+++ main/java/com/example/vermia/cosas/Idiomas.java
@@ -0,0 +1,105 @@
+package com.example.vermia.cosas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+
+public class Idiomas extends AppCompatActivity {
+    private RadioButton boton_espanol, boton_aleman, boton_frances, boton_ingles, boton_italiano;
+    private static String idioma;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_idiomas);
+
+        boton_aleman= (RadioButton) findViewById(R.id.idioma_aleman_boton);
+        boton_espanol= (RadioButton) findViewById(R.id.idioma_espanol_boton);
+        boton_frances =(RadioButton) findViewById(R.id.idioma_frances_boton);
+        boton_ingles =(RadioButton) findViewById(R.id.idioma_ingles_boton);
+        boton_italiano =(RadioButton) findViewById(R.id.idioma_italiano_boton);
+
+        boton_espanol.setChecked(true);
+
+
+    }
+
+    public void pulsaAleman(View v) {
+        boton_aleman.setChecked(true);
+
+        if(boton_espanol.isChecked())
+            boton_espanol.setChecked(false);
+        if(boton_frances.isChecked())
+            boton_frances.setChecked(false);
+        if(boton_ingles.isChecked())
+            boton_ingles.setChecked(false);
+        if(boton_italiano.isChecked())
+            boton_italiano.setChecked(false);
+
+        idioma=new String("Aleman");
+    }
+
+    public void pulsaEspanol(View v){
+        boton_espanol.setChecked(true);
+
+        if(boton_aleman.isChecked())
+            boton_aleman.setChecked(false);
+        if(boton_frances.isChecked())
+            boton_frances.setChecked(false);
+        if(boton_ingles.isChecked())
+            boton_ingles.setChecked(false);
+        if(boton_italiano.isChecked())
+            boton_italiano.setChecked(false);
+
+        idioma=new String("Espanol");
+    }
+
+    public void pulsaFrances(View v) {
+        boton_frances.setChecked(true);
+
+        if(boton_espanol.isChecked())
+            boton_espanol.setChecked(false);
+        if(boton_aleman.isChecked())
+            boton_aleman.setChecked(false);
+        if(boton_ingles.isChecked())
+            boton_ingles.setChecked(false);
+        if(boton_italiano.isChecked())
+            boton_italiano.setChecked(false);
+
+        idioma=new String("Frances");
+    }
+
+    public void pulsaIngles(View v){
+        boton_ingles.setChecked(true);
+
+        if(boton_espanol.isChecked())
+            boton_espanol.setChecked(false);
+        if(boton_frances.isChecked())
+            boton_frances.setChecked(false);
+        if(boton_aleman.isChecked())
+            boton_aleman.setChecked(false);
+        if(boton_italiano.isChecked())
+            boton_italiano.setChecked(false);
+
+        idioma=new String("Ingles");
+    }
+
+    public void pulsaItaliano(View v){
+        boton_italiano.setChecked(true);
+
+        if(boton_espanol.isChecked())
+            boton_espanol.setChecked(false);
+        if(boton_frances.isChecked())
+            boton_frances.setChecked(false);
+        if(boton_aleman.isChecked())
+            boton_aleman.setChecked(false);
+        if(boton_ingles.isChecked())
+            boton_ingles.setChecked(false);
+
+        idioma=new String("Italiano");
+    }
+
+}
+
---
+++ main/java/com/example/vermia/cosas/MainActivity.java
@@ -0,0 +1,131 @@
+package com.example.vermia.cosas;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+
+import static android.view.View.GONE;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_filtros) {
+            Intent filtros=new Intent(getApplicationContext(),Filtros.class);
+            startActivity(filtros);
+        } else if (id == R.id.nav_idiomas) {
+            Intent idiomas= new Intent(getApplicationContext(),Idiomas.class);
+            startActivity(idiomas);
+        } else if (id == R.id.nav_temas) {
+            Intent temas = new Intent(getApplicationContext(),Temas.class);
+            startActivity(temas);
+        } else if (id == R.id.nav_about) {
+            Intent about= new Intent(getApplicationContext(),About.class);
+            startActivity(about);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+
+    public void despliegaSeries(View v){
+        LinearLayout listaSeries=(LinearLayout)findViewById(R.id.listaSeries);
+
+        if(listaSeries.getVisibility()==GONE)
+            listaSeries.setVisibility(View.VISIBLE);
+        else listaSeries.setVisibility(GONE);
+    }
+
+    public void despliegaPeliculas(View v){
+        LinearLayout listaPeliculas=(LinearLayout)findViewById(R.id.listaPeliculas);
+
+        if(listaPeliculas.getVisibility()==GONE)
+            listaPeliculas.setVisibility(View.VISIBLE);
+        else listaPeliculas.setVisibility(GONE);
+    }
+}
