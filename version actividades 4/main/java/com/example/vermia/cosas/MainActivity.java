da6d0f7d73ca170f33377b2196bba68877eaa8c6
iCbsp <33635733+iCbsp@users.noreply.github.com>
2017-12-09 19:21:20 +0100

    buscador 3

--- version actividades 4/main/java/com/example/vermia/cosas/MainActivity.java
+++ version actividades 4/main/java/com/example/vermia/cosas/MainActivity.java
@@ -48,14 +48,7 @@ public class MainActivity extends AppCompatActivity
 
         BusquedaInicio = (EditText) findViewById(R.id.BusquedaInicio);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
