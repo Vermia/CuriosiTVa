6eb894573151ae399d77c984197fe1cb088392e1
iCbsp <33635733+iCbsp@users.noreply.github.com>
2017-12-10 13:52:22 +0100

    La version 8

---
+++ Actividades 8/main/java/com/example/vermia/cosas/Video1x01.java
@@ -0,0 +1,72 @@
+package com.example.vermia.cosas;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static android.R.drawable.ic_media_play;
+import static android.view.View.GONE;
+
+public class Video1x01 extends AppCompatActivity {
+
+    private int progressStatus = 0;
+    private boolean pausa;
+    private Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video1x01);
+
+        ImageButton botonPausa= (ImageButton) findViewById(R.id.botonPausa);
+        botonPausa.setImageResource(R.mipmap.imagen_play);
+
+        pausa=true;
+        play();
+    }
+
+    public void pulsaPausa(View v){
+        ImageButton botonPausa= (ImageButton) findViewById(R.id.botonPausa);
+        if(pausa){
+            pausa=false;
+            botonPausa.setImageResource(R.mipmap.imagen_pausa);
+        }else{
+            pausa=true;
+            botonPausa.setImageResource(R.mipmap.imagen_play);
+        }
+    }
+
+    public void play(){
+        final SeekBar barra=(SeekBar) findViewById(R.id.seekBar);
+        final LinearLayout infonull=(LinearLayout) findViewById(R.id.InfoNull);
+
+        barra.setMax(60*22);
+        int delay = 0; // delay for 0 sec.
+        int period = 10;
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask()
+        {
+            public void run()
+            {
+                if(!pausa){
+                    if(barra.getProgress()==50){
+                        infonull.setVisibility(GONE);
+                    }
+                    barra.setProgress(barra.getProgress()+1);
+
+                }
+            }
+        }, delay, period);
+    }
+
+}
