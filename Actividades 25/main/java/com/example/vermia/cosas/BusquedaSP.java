bace4d5d8661248ded9af512df5b22edc14301d9
iCbsp <33635733+iCbsp@users.noreply.github.com>
2017-12-15 14:02:03 +0100

    pequenos cambios en 25

--- Actividades 25/main/java/com/example/vermia/cosas/BusquedaSP.java
+++ Actividades 25/main/java/com/example/vermia/cosas/BusquedaSP.java
@@ -7,9 +7,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
 
 /**
  * Created by Carlos BSP on 09/12/2017.
@@ -19,6 +21,10 @@ public class BusquedaSP extends AppCompatActivity {
 
 
     private TextView nombre, Serie1, Serie2, Serie3, Pelicula1, Pelicula2, Pelicula3, Resultados;
+
+
+    private ImageButton filtros, idiomas, about, menudragon, temas, home;
+    private RadioGroup barra;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +55,118 @@ public class BusquedaSP extends AppCompatActivity {
 
 
 
+        barra = (RadioGroup) findViewById(R.id.barra);
+
+        home = (ImageButton) findViewById(R.id.home);
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent BusquedaInicio= new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(BusquedaInicio);
+                finish();
+
+            }
+        });
+
+        filtros = (ImageButton) findViewById(R.id.filtros);
+
+        filtros.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent BusquedaInicio= new Intent(getApplicationContext(),Filtros.class);
+                startActivity(BusquedaInicio);
+                finish();
+
+            }
+        });
+
+        idiomas = (ImageButton) findViewById(R.id.idiomas);
+
+        idiomas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent BusquedaInicio= new Intent(getApplicationContext(),Idiomas.class);
+                startActivity(BusquedaInicio);
+                finish();
+
+            }
+        });
+
+        about = (ImageButton) findViewById(R.id.about);
+
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent BusquedaInicio= new Intent(getApplicationContext(),About.class);
+                startActivity(BusquedaInicio);
+                finish();
+
+            }
+        });
+
+        temas = (ImageButton) findViewById(R.id.temas);
+
+        temas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent BusquedaInicio= new Intent(getApplicationContext(),Temas.class);
+                startActivity(BusquedaInicio);
+                finish();
+
+            }
+        });
+
+        menudragon = (ImageButton) findViewById(R.id.imagindragon);
+
+        menudragon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(about.getVisibility()==GONE){
+                    about.setVisibility(VISIBLE);
+                }
+                else{
+                    about.setVisibility(GONE);
+                }
+
+                if(filtros.getVisibility()==GONE){
+                    filtros.setVisibility(VISIBLE);
+                }
+                else{
+                    filtros.setVisibility(GONE);
+                }
+
+                if(idiomas.getVisibility()==GONE){
+                    idiomas.setVisibility(VISIBLE);
+                }
+                else{
+                    idiomas.setVisibility(GONE);
+                }
+
+                if(temas.getVisibility()==GONE){
+                    temas.setVisibility(VISIBLE);
+                }
+                else{
+                    temas.setVisibility(GONE);
+                }
+
+                if(home.getVisibility()==GONE){
+                    home.setVisibility(VISIBLE);
+                }
+                else{
+                    home.setVisibility(GONE);
+                }
+            }
+        });
+
+
+        barra.setBackgroundColor(Temas.getCol());
+
     }
 
     private void Busqueda(){
@@ -61,42 +179,42 @@ public class BusquedaSP extends AppCompatActivity {
 
             if ("the big bang theory".contains(busq) || "THE BIG BANG THEORY".contains(busq)) {
 
-                Serie1.setVisibility(View.VISIBLE);
+                Serie1.setVisibility(VISIBLE);
                 cont++;
 
             }
 
             if ("juego de tronos".contains(busq) || "JUEGO DE TRONOS".contains(busq) || "game of thrones".contains(busq) || "GAME OF THRONES".contains(busq)) {
 
-                Serie2.setVisibility(View.VISIBLE);
+                Serie2.setVisibility(VISIBLE);
                 cont++;
 
             }
 
             if ("rick y morty".contains(busq) || "RICK Y MORTY".contains(busq) || "rick and morty".contains(busq) || "RICK AND MORTY".contains(busq)) {
 
-                Serie3.setVisibility(View.VISIBLE);
+                Serie3.setVisibility(VISIBLE);
                 cont++;
 
             }
 
             if ("transformers".contains(busq) || "TRANSFORMERS".contains(busq)) {
 
-                Pelicula1.setVisibility(View.VISIBLE);
+                Pelicula1.setVisibility(VISIBLE);
                 cont++;
 
             }
 
             if ("origen".contains(busq) || "ORIGEN".contains(busq) || "inception".contains(busq) || "INCEPTION".contains(busq)) {
 
-                Pelicula2.setVisibility(View.VISIBLE);
+                Pelicula2.setVisibility(VISIBLE);
                 cont++;
 
             }
 
             if ("paranormal activity".contains(busq) || "PARANORMAL ACTIVITY".contains(busq)) {
 
-                Pelicula3.setVisibility(View.VISIBLE);
+                Pelicula3.setVisibility(VISIBLE);
                 cont++;
 
             }
--- Actividades 25/main/java/com/example/vermia/cosas/BusquedaTBBT.java
+++ Actividades 25/main/java/com/example/vermia/cosas/BusquedaTBBT.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
 
 /**
  * Created by Carlos BSP on 09/12/2017.
@@ -20,6 +23,11 @@ public class BusquedaTBBT extends AppCompatActivity {
             cap3x01, cap3x02, cap3x03, cap4x01, cap4x02, cap4x03, cap5x01, cap5x02, cap5x03, Resultados;
 
     private Button bot;
+
+
+    private ImageButton filtros, idiomas, about, menudragon, temas, home;
+    private RadioGroup barra;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -88,6 +96,117 @@ public class BusquedaTBBT extends AppCompatActivity {
 
         busqTBBTueda();
 
+
+        barra = (RadioGroup) findViewById(R.id.barra);
+
+        home = (ImageButton) findViewById(R.id.home);
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent BusquedaInicio= new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(BusquedaInicio);
+                finish();
+
+            }
+        });
+
+        filtros = (ImageButton) findViewById(R.id.filtros);
+
+        filtros.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent BusquedaInicio= new Intent(getApplicationContext(),Filtros.class);
+                startActivity(BusquedaInicio);
+                finish();
+
+            }
+        });
+
+        idiomas = (ImageButton) findViewById(R.id.idiomas);
+
+        idiomas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent BusquedaInicio= new Intent(getApplicationContext(),Idiomas.class);
+                startActivity(BusquedaInicio);
+                finish();
+
+            }
+        });
+
+        about = (ImageButton) findViewById(R.id.about);
+
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent BusquedaInicio= new Intent(getApplicationContext(),About.class);
+                startActivity(BusquedaInicio);
+                finish();
+
+            }
+        });
+
+        temas = (ImageButton) findViewById(R.id.temas);
+
+        temas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent BusquedaInicio= new Intent(getApplicationContext(),Temas.class);
+                startActivity(BusquedaInicio);
+                finish();
+
+            }
+        });
+
+        menudragon = (ImageButton) findViewById(R.id.imagindragon);
+
+        menudragon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(about.getVisibility()==GONE){
+                    about.setVisibility(VISIBLE);
+                }
+                else{
+                    about.setVisibility(GONE);
+                }
+
+                if(filtros.getVisibility()==GONE){
+                    filtros.setVisibility(VISIBLE);
+                }
+                else{
+                    filtros.setVisibility(GONE);
+                }
+
+                if(idiomas.getVisibility()==GONE){
+                    idiomas.setVisibility(VISIBLE);
+                }
+                else{
+                    idiomas.setVisibility(GONE);
+                }
+
+                if(temas.getVisibility()==GONE){
+                    temas.setVisibility(VISIBLE);
+                }
+                else{
+                    temas.setVisibility(GONE);
+                }
+
+                if(home.getVisibility()==GONE){
+                    home.setVisibility(VISIBLE);
+                }
+                else{
+                    home.setVisibility(GONE);
+                }
+            }
+        });
+
+        barra.setBackgroundColor(Temas.getCol());
     }
 
     private void busqTBBTueda(){
@@ -100,56 +219,56 @@ public class BusquedaTBBT extends AppCompatActivity {
 
             if ("01 piloto".contains(busqTBBT) || "01 PILOTO".contains(busqTBBT) || "01 pilot".contains(busqTBBT) || "01 PILOT".contains(busqTBBT)) {
 
-                cap1x01.setVisibility(View.VISIBLE);
+                cap1x01.setVisibility(VISIBLE);
                 cont++;
 
             }
 
             if ("02 la hipótesis del gran cerebro".contains(busqTBBT) || "02 LA HIPÓTESIS DEL GRAN CEREBRO".contains(busqTBBT) || "02 THE BIG BRAN HYPOTHESIS".contains(busqTBBT) || "02 the big bran hypothesis".contains(busqTBBT)) {
 
-                cap1x02.setVisibility(View.VISIBLE);
+                cap1x02.setVisibility(VISIBLE);
                 cont++;
 
             }
 
             if ("03 el corolario de botas peludas".contains(busqTBBT) || "03 EL COROLARIO DE BOTAS PELUDAS".contains(busqTBBT) || "03 the fuzzy boots corollary".contains(busqTBBT) || "03 THE FUZZY BOOTS COROLLARY".contains(busqTBBT)) {
 
-                cap1x03.setVisibility(View.VISIBLE);
+                cap1x03.setVisibility(VISIBLE);
                 cont++;
 
             }
 
             if ("04 el efecto del pez luminoso".contains(busqTBBT) || "04 EL EFECTO DEL PEZ LUMINOSO".contains(busqTBBT) || "04 the luminous fish effect".contains(busqTBBT) || "04 THE LUMINOUS FISH EFFECT".contains(busqTBBT)) {
 
-                cap1x04.setVisibility(View.VISIBLE);
+                cap1x04.setVisibility(VISIBLE);
                 cont++;
 
             }
             //TEMPORADA 2
             if ("01 el paradigma del pescado malo".contains(busqTBBT) || "01 EL PARADIGMA DEL PESCADO MALO".contains(busqTBBT) || "01 the bad fish paradigm".contains(busqTBBT) || "01 THE BAD FISH PARADIGM".contains(busqTBBT)) {
 
-                cap2x01.setVisibility(View.VISIBLE);
+                cap2x01.setVisibility(VISIBLE);
                 cont++;
 
             }
 
             if ("02 the codpiece topology".contains(busqTBBT) || "02 THE CODPIECE TOPOLOGY".contains(busqTBBT) || "02 la topología codpiece".contains(busqTBBT) || "02 LA TOPOLOGÍA CODPIECE".contains(busqTBBT)) {
 
-                cap2x02.setVisibility(View.VISIBLE);
+                cap2x02.setVisibility(VISIBLE);
                 cont++;
 
             }
 
             if ("03 the barbarian sublimation".contains(busqTBBT) || "03 THE BARBARIAN SUBLIMATION".contains(busqTBBT) || "03 la sublimación bárbara".contains(busqTBBT) || "03 LA SUBLIMACIÓN BÁRBARA".contains(busqTBBT)) {
 
-                cap2x03.setVisibility(View.VISIBLE);
+                cap2x03.setVisibility(VISIBLE);
                 cont++;
 
             }
 
             if ("04 la equivalencia del grifo".contains(busqTBBT) || "04 LA EQUIVALENCIA DEL GRIFO".contains(busqTBBT) || "04 the griffin equivalency".contains(busqTBBT) || "04 THE GRIFFIN EQUIVALENCY".contains(busqTBBT)) {
 
-                cap2x04.setVisibility(View.VISIBLE);
+                cap2x04.setVisibility(VISIBLE);
                 cont++;
 
             }
@@ -158,21 +277,21 @@ public class BusquedaTBBT extends AppCompatActivity {
 
             if ("01 la fluctuación del abrelatas eléctrico".contains(busqTBBT) || "01 LA FLUCTUACIÓN DEL ABRELATAS ELÉCTRICO".contains(busqTBBT) || "01 the electric can opener fluctuation".contains(busqTBBT) || "01 THE ELECTRIC CAN OPENER FLUCTUATION".contains(busqTBBT)) {
 
-                cap3x01.setVisibility(View.VISIBLE);
+                cap3x01.setVisibility(VISIBLE);
                 cont++;
 
             }
 
             if ("02 las conjeturas de jiminy".contains(busqTBBT) || "02 LAS CONJETURAS DE JIMINY".contains(busqTBBT) || "02 the jiminy conjeture".contains(busqTBBT) || "02 THE JIMINY CONJETURE".contains(busqTBBT)) {
 
-                cap3x02.setVisibility(View.VISIBLE);
+                cap3x02.setVisibility(VISIBLE);
                 cont++;
 
             }
 
             if ("03 la variante gothowitz".contains(busqTBBT) || "03 LA VARIANTE GOTHOWITZ".contains(busqTBBT) || "03 the gothowitz variation".contains(busqTBBT) || "03 THE GOTHOWITZ VARIATION".contains(busqTBBT)) {
 
-                cap3x03.setVisibility(View.VISIBLE);
+                cap3x03.setVisibility(VISIBLE);
                 cont++;
 
             }
@@ -181,21 +300,21 @@ public class BusquedaTBBT extends AppCompatActivity {
 
             if ("01 manipulación robótica".contains(busqTBBT) || "01 MANIPULACIÓN ROBÓTICA".contains(busqTBBT) || "01 the robotic manipulation".contains(busqTBBT) || "01 THE ROBOTIC MANIPULATION".contains(busqTBBT)) {
 
-                cap4x01.setVisibility(View.VISIBLE);
+                cap4x01.setVisibility(VISIBLE);
                 cont++;
 
             }
 
             if ("02 la amplificación de verduras".contains(busqTBBT) || "02 LA AMPLIFICACIÓN DE VERDURAS".contains(busqTBBT) || "02 THE CRUCIFEROUS VEGETABLE AMPLIFICATION".contains(busqTBBT) || "02 the cruciferous vegetable amplification".contains(busqTBBT)) {
 
-                cap4x02.setVisibility(View.VISIBLE);
+                cap4x02.setVisibility(VISIBLE);
                 cont++;
 
             }
 
             if ("03 una sustitucion brillante".contains(busqTBBT) || "03 UNA SUSTITUCION BRILLANTE".contains(busqTBBT) || "03 the zazzy substitution".contains(busqTBBT) || "03 THE ZAZZY SUBSTITUTION".contains(busqTBBT)) {
 
-                cap4x03.setVisibility(View.VISIBLE);
+                cap4x03.setVisibility(VISIBLE);
                 cont++;
 
             }
@@ -205,21 +324,21 @@ public class BusquedaTBBT extends AppCompatActivity {
 
             if ("01 analisis del reflejo de golfa".contains(busqTBBT) || "01 ANALISIS DEL REFLEJO DE GOLFA".contains(busqTBBT) || "01 the skank reflex analysis".contains(busqTBBT) || "01 THE SKANS REFLEX ANALISYS".contains(busqTBBT)) {
 
-                cap5x01.setVisibility(View.VISIBLE);
+                cap5x01.setVisibility(VISIBLE);
                 cont++;
 
             }
 
             if ("02 la teoría de la plaga".contains(busqTBBT) || "02 LA TEORÍA DE LA PLAGA".contains(busqTBBT) || "02 the infestation hypothesis".contains(busqTBBT) || "02 THE INFESTATION HYPOTHESIS".contains(busqTBBT)) {
 
-                cap5x02.setVisibility(View.VISIBLE);
+                cap5x02.setVisibility(VISIBLE);
                 cont++;
 
             }
 
             if ("03 la extrapolación del tirón de ingle".contains(busqTBBT) || "03 LA EXTRAPOLACIÓN DEL TIRÓN DE INGLE".contains(busqTBBT) || "03 the pulled groin extrapolation".contains(busqTBBT) || "03 THE PULLED GROIN EXTRAPOLATION".contains(busqTBBT)) {
 
-                cap5x03.setVisibility(View.VISIBLE);
+                cap5x03.setVisibility(VISIBLE);
                 cont++;
 
             }
