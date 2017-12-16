a2b638f23853361a5c4782d49b06c2bc01f04bd0
Vermia <33635769+Vermia@users.noreply.github.com>
2017-12-16 12:14:32 +0100

    EL PUTO LAYOUT DE VIDEO

---
+++ Actividades 30/main/java/com/example/vermia/cosas/Introduccion.java
@@ -0,0 +1,46 @@
+package com.example.vermia.cosas;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class Introduccion extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Idiomas.setIdioma("Espanol");
+
+        Temas.setColor("Azul");
+
+        Temas.setCol(getResources().getColor(R.color.colorPrimary));
+
+        Filtros.setActores(true);
+        Filtros.setRopa(true);
+        Filtros.setArticulos(true);
+        Filtros.setTerminos(true);
+        Filtros.setLugares(true);
+        Filtros.setPersonajes(true);
+
+
+        setContentView(R.layout.intro);
+
+        Runnable r = new Runnable() {
+            @Override
+            public void run(){
+
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+
+            }
+        };
+
+        Handler h = new Handler();
+        h.postDelayed(r, 650);
+
+
+    }
+}
---
+++ Actividades 30/main/java/com/example/vermia/cosas/Main2Activity.java
@@ -0,0 +1,23 @@
+package com.example.vermia.cosas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class Main2Activity extends AppCompatActivity {
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        LinearLayout lin=(LinearLayout)findViewById(R.id.listaSeries);
+
+    }
+
+
+
+}
